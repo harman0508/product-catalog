c9caa9de85ada3ee58fa12d0e9add5b20e03a57a
harman0508 <harman.iips@gmail.com>
2026-05-04 18:04:06 +0530

    Delete backend directory

--- backend/catalog/tests/test_models.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from catalog.models import Category
-
-
-class ModelTest(TestCase):
-
-    def test_category(self):
-        c = Category.objects.create(name="Test")
-        self.assertEqual(str(c), "Test")
